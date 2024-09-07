55997da483b419269b94ab67a6a39771d50c9fc3
Davide Lombardi <davidelombardi130p@gmail.com>
2024-09-07 12:48:54 +0200

    wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,11 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.font_size = 13.5
+config.color_scheme = "Catppuccin Macchiato"
+
+-- and finally, return the configuration to wezterm
+return config
